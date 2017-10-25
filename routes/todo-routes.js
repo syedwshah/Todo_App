8c6083c8092645fe0e0d22dc1d6d2b8ad87c3c91
Syed Shah <syed.shah@nyu.edu>
2017-10-25 15:33:47 -0400

    Create todo-routes.js

---
+++ routes/todo-routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const todoRoutes = express.Router();
+const todoController = require('../controllers/pokemon-controller');
+
+todoRoutes.get('/', todoController.index);
+// todoRoutes.post('/', todoController.create);
+
+// todoRoutes.get('/add', (req,res)=>{
+//   res.render('./todo-add');
+// })
+
+// todoRoutes.get('/:id', todoController.show);
+// todoRoutes.put('/', todoController.update);
+// todoRoutes.delete('/:id', todoController.delete);
+
+module.exports = todoRoutes;
