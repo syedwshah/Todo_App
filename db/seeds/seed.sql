2aaf454dbeab32009fa6551d84bda14b5b0daeb6
Syed Shah <syed.shah@nyu.edu>
2017-10-25 15:18:05 -0400

    Fix potential errors in controller, Set up a seed.sql for testing

---
+++ db/seeds/seed.sql
@@ -0,0 +1,1 @@
+\i todo.sql;
---
+++ db/seeds/todo.sql
@@ -0,0 +1,8 @@
+INSERT INTO todo_sql (task, deadline, location, priority, complete) VALUES
+  (
+    'Complete todo app asignment',
+    '10/26/2017 by 11pm',
+    'GA, but upload to personal github and make a pull req. to GA github enterprise',
+    '10',
+    'false'
+  );
